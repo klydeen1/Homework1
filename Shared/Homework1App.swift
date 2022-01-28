c90af5bbc2ee5edff3b613a1a4e5b21ec9d7775b
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-01-28 15:57:29 -0600

    Initial Commit

---
+++ Shared/Homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework1App.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
